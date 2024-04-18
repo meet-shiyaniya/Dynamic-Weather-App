a498524e15fe1598f4da15f2319928f91b670ccd
Meet Patel <138037485+meet-shiyaniya@users.noreply.github.com>
2024-04-18 19:12:10 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,46 @@
+const dotenv = require("dotenv");
+const http = require("http");
+const fs = require("fs");
+var requests = require("requests");
+
+dotenv.config();
+const homeFile = fs.readFileSync("home.html", "utf-8");
+
+const replaceVal = (tempVal, orgVal) => {
+  let temperature = tempVal.replace("{%tempval%}", orgVal.main.temp);
+  temperature = temperature.replace("{%tempmin%}", orgVal.main.temp_min);
+  temperature = temperature.replace("{%tempmax%}", orgVal.main.temp_max);
+  temperature = temperature.replace("{%location%}", orgVal.name);
+  temperature = temperature.replace("{%country%}", orgVal.sys.country);
+  temperature = temperature.replace("{%tempstatus%}", orgVal.weather[0].main);
+
+  return temperature;
+};
+
+const server = http.createServer((req, res) => {
+  if (req.url == "/") {
+    requests(
+      "http://api.openweathermap.org/data/2.5/weather?q=surat&appid=bf116f4b45c39436227f5b685e0b80b2"
+    )
+      .on("data", (chunk) => {
+        const objdata = JSON.parse(chunk);
+        const arrData = [objdata];
+        // console.log(arrData[0].main.temp);
+        const realTimeData = arrData
+          .map((val) => replaceVal(homeFile, val))
+          .join("");
+        res.write(realTimeData);
+        // console.log(realTimeData);
+      })
+      .on("end", (err) => {
+        if (err) return console.log("connection closed due to errors", err);
+        res.end();
+      });
+  } else {
+    res.end("File not found");
+  }
+});
+
+server.listen(process.env.PORT, () => {
+  console.log("You Are Running On Port No: " + process.env.PORT);
+});
